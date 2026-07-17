e4036bd423f4add14666168c1de66f0a6375446c
nopalllfd <Naufalfadhilah689@gmail.com>
2026-07-17 14:27:28 +0700

    style: add numbering

--- sql/asnwer.sql
+++ sql/asnwer.sql
@@ -1,3 +1,4 @@
+#1
 INSERT INTO employees
 (name, position, join_date, years_of_experience, salary)
 VALUES
@@ -9,25 +10,29 @@ VALUES
     50
 );
 
+#2
 UPDATE employees
 SET salary = 85
 WHERE position = 'Engineer';
 
+#3
 SELECT 
     SUM(salary) AS total_salary_2021
 FROM employees
 WHERE 
-    join_date <= '2021-12-31'
+    (join_date <= '2021-12-31' AND join_date => '2021-01-01')
     AND (
         release_date IS NULL 
         OR release_date >= '2021-01-01'
     );
 
-    SELECT *
+#4
+SELECT *
 FROM employees
 ORDER BY years_of_experience DESC
 LIMIT 3;
 
+#5
 SELECT *
 FROM employees
 WHERE id IN (
