2d1ee1520df2b1c0bfe314142a4071f7aa28beda
nopalllfd <Naufalfadhilah689@gmail.com>
2026-07-20 11:04:36 +0700

    fix: typo

--- sql/asnwer.sql
+++ sql/asnwer.sql
@@ -20,7 +20,7 @@ SELECT
     SUM(salary) AS total_salary_2021
 FROM employees
 WHERE 
-    (join_date <= '2021-12-31' AND join_date => '2021-01-01')
+    (join_date <= '2021-12-31' AND join_date >= '2021-01-01')
     AND (
         release_date IS NULL 
         OR release_date >= '2021-01-01'
